a5c83b6fd6500d0f0fd7c26528e0a9a36840aefe
Isaac Rodríguez <edgar.rodriguez@docsolutions.com>
2024-12-31 11:00:36 -0700

    add 2 numbers

--- Sumar2Numeros/actions/actions.py
+++ Sumar2Numeros/actions/actions.py
@@ -25,3 +25,56 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionSumarNumeros(Action):
+    def name(self) -> Text:
+        return "action_sumar_numeros"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        # Obtener los números del tracker
+        # numero1 = tracker.get_slot("numero1")
+
+        numero1_slot = next(tracker.get_latest_entity_values("numero1"), None)
+        print (f"numero1_slot: {numero1_slot}")
+
+        numero1 = tracker.get_slot("numero1")
+        print (f"numero1: {numero1}")
+
+        numero1 = numero1 or numero1_slot
+
+        numero2_slot = next(tracker.get_latest_entity_values("numero2"), None)
+        print (f"numero2_slot: {numero2_slot}")
+
+        numero2 = tracker.get_slot("numero2")
+        print (f"numero2: {numero2}")
+
+        numero2 = numero2 or numero2_slot
+        
+        if numero1 is None:
+            dispatcher.utter_message(text="No puedo realizar la suma porque me falta el número 1.")
+            return []
+        
+        if numero2 is None:
+            dispatcher.utter_message(text="No puedo realizar la suma porque me falt el número 2.")
+            return []
+
+        # Convertir a flotante y realizar la suma
+        resultado = float(numero1) + float(numero2)
+
+        # Enviar el resultado al usuario
+        dispatcher.utter_message(text=f"El resultado de sumar {numero1} y {numero2} es {resultado}.")
+
+        # Guardar el resultado en un slot (opcional)
+        return [SlotSet("resultado", resultado)]
